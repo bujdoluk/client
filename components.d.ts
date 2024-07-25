9b4e74c85b8b22c280d87fa5dc7b292ea547f500
bujdoluk <lukas.bujdos@gmail.com>
2024-07-25 18:40:15 +0200

    ci: fixed configs, installed cypress

--- components.d.ts
+++ components.d.ts
@@ -8,14 +8,10 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AddFeedback: typeof import('./src/components/Dialogs/AddFeedback.vue')['default']
-    AddFeedbackDialog: typeof import('./src/components/Dialogs/AddFeedbackDialog.vue')['default']
     AppToolbar: typeof import('./src/components/AppToolbar/AppToolbar.vue')['default']
     EditFeedback: typeof import('./src/components/Dialogs/EditFeedback.vue')['default']
-    EditFeedbackDialog: typeof import('./src/components/Dialogs/EditFeedbackDialog.vue')['default']
     EmptyFeedbackComponent: typeof import('./src/components/Feedback/EmptyFeedbackComponent.vue')['default']
-    Feedback: typeof import('./src/components/Feedback/FeedbackComponent.vue')['default']
     FeedbackCard: typeof import('./src/components/FeedbackCard/FeedbackCard.vue')['default']
-    FeedbackDialog: typeof import('./src/components/FeedbackDialog.vue')['default']
     FrontendMentorBox: typeof import('./src/components/FrontendMentorBox/FrontendMentorBox.vue')['default']
     GoBackButton: typeof import('./src/components/GoBackButton/GoBackButton.vue')['default']
     LoaderOverlay: typeof import('./src/components/LoaderOverlay/LoaderOverlay.vue')['default']
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { type ConfigEnv, type UserConfig, defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { type ConfigEnv, type UserConfig, defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
@@ -7,52 +7,52 @@ import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const config = (env: ConfigEnv): UserConfig => {
-  process.env = { ...process.env, ...loadEnv(env.mode, process.cwd()) };
-  const base = String(process.env.VITE_APP_BASE);
-  const host = String(process.env.VITE_APP_HOST);
-  const port = Number(process.env.VITE_APP_PORT);
+    process.env = { ...process.env, ...loadEnv(env.mode, process.cwd()) };
+    const base = String(process.env.VITE_APP_BASE);
+    const host = String(process.env.VITE_APP_HOST);
+    const port = Number(process.env.VITE_APP_PORT);
 
-  const config: UserConfig = {
-    base,
-    build: {
-      sourcemap: 'hidden',
-      target: 'ESNext'
-    },
-    define: {
-      'process.env': process.env
-    },
-    plugins: [
-      VueI18nPlugin({
-        include: [resolve(__dirname, './src/locales/**')]
-      }),
-      vue({ template: { transformAssetUrls } }),
-      vuetify({ autoImport: true }),
-      Components({ dts: true })
-    ],
-    preview: {
-      host,
-      port,
-      strictPort: true
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
-      extensions: [
-        '.json',
-        '.js',
-        '.mjs',
-        '.ts',
-        '.vue'
-      ]
-    },
-    server: {
-      host,
-      port,
-      strictPort: true
-    }
-  };
-  return config;
+    const config: UserConfig = {
+        base,
+        build: {
+            sourcemap: 'hidden',
+            target: 'ESNext'
+        },
+        define: {
+            'process.env': process.env
+        },
+        plugins: [
+            VueI18nPlugin({
+                include: [resolve(__dirname, './src/locales/**')]
+            }),
+            vue({ template: { transformAssetUrls } }),
+            vuetify({ autoImport: true }),
+            Components({ dts: true })
+        ],
+        preview: {
+            host,
+            port,
+            strictPort: true
+        },
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            },
+            extensions: [
+                '.json',
+                '.js',
+                '.mjs',
+                '.ts',
+                '.vue'
+            ]
+        },
+        server: {
+            host,
+            port,
+            strictPort: true
+        }
+    };
+    return config;
 };
 
 export default defineConfig((env: ConfigEnv) => config(env));
