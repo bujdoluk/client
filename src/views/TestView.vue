c7de2e1ea2f1c399d19a7d672db35474f2c9de05
bujdoluk <lukas.bujdos@gmail.com>
2024-08-19 15:45:48 +0200

    fix: test commit delete some stuff

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -8,18 +8,11 @@ import { ref, watch } from 'vue';
 import { db, auth } from '@/firebase/init';
 
 const user = ref(auth().currentUser);
-const users = ref<Array<string>>();
 
 export interface User {
     email: string;
     userName: string;
 }
-
-// const users = db.collection('users');
-// const user = db.collection('users').doc(userId);
-const userfeedbacks = db.collection('users').doc(user.value?.uid).collection('feedbacks');
-const userComments = db.collection('users').doc(user.value?.uid).collection('comments');
-const userUpvotes = db.collection('users').doc(user.value?.uid).collection('upvotes');
 
 watch((user), async (): Promise<void> => {
     if (user.value !== null) {
